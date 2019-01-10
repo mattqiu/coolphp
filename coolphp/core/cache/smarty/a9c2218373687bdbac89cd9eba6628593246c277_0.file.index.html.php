c1e5483ff277aec6e5e2387c8b1f39ff8389f3c0
2daye <839595028@qq.com>
2019-01-10 15:04:35 +0800

    V1.1

---
+++ coolphp/core/cache/smarty/a9c2218373687bdbac89cd9eba6628593246c277_0.file.index.html.php
@@ -0,0 +1,38 @@
+<?php
+/* Smarty version 3.1.31, created on 2019-01-10 14:55:08
+  from "C:\wamp\www\c\core\app\index\view\index.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.31',
+  'unifunc' => 'content_5c36ec4cda7ac9_63996795',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'a9c2218373687bdbac89cd9eba6628593246c277' => 
+    array (
+      0 => 'C:\\wamp\\www\\c\\core\\app\\index\\view\\index.html',
+      1 => 1547102842,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5c36ec4cda7ac9_63996795 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>欢迎使用 CoolPhp框架</title>
+</head>
+<body>
+<h1 style="margin: 40px;font-size: 80px;"><?php echo $_smarty_tpl->tpl_vars['smile']->value;?>
+</h1>
+<p style="margin: 40px;font-size: 25px;"><?php echo $_smarty_tpl->tpl_vars['welcome']->value;?>
+</p>
+</body>
+</html><?php }
+}
