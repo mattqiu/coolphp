7683103042bf90368e0911e580dfe9c7324115e1
2daye <839595028@qq.com>
2019-01-16 14:56:04 +0800

    刪除了

--- coolphp/core/config/cache.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-    'FILE_CACHE' => [
-        'PATH' => '/core/cache/file_cache/'
-    ],
-    'MEMCACHED_CACHE' => []
-);
--- coolphp/core/config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'DB_HOST' => 'localhost',
-    'DB_USER' => 'root',
-    'DB_NAME' => 'hahaha',
-    'DB_PASSWORD' => '123456',
-    'DB_PORT_NUMBER' => '3306'
-);
